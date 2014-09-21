942a84d899bbc0eaaa940d5f7fe5bed177290dc8
Quentin <quentin+github@fourel.eu>
2014-09-21 15:27:05 +0200

    Ajout envoi d'un mail de fin

--- FiBa.py
+++ FiBa.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, ConfigParser, tarfile, socket, time, scp, paramiko
+import os, ConfigParser, tarfile, socket, time, scp, paramiko, smtplib, sys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 config = ConfigParser.ConfigParser()
 config.read("FiBa.conf")
@@ -15,6 +17,9 @@ for s in config.sections():
         serverPort = int(config.get(s, 'Port'))
         serverUser = config.get(s, 'User')
         serverKey = paramiko.RSAKey.from_private_key_file(config.get(s, 'Key'))
+    elif s == 'Email':
+        sender = config.get(s, 'Sender')
+        receiver = config.get(s, 'Receiver')
     elif s == 'Archive':
         archiveName = '/tmp/' + config.get(s, 'Name') + '_' + socket.gethostname().split('.')[0] + '_' + time.strftime("%Y%m%d%H%M%S") + '.tar.gz'
     else:
@@ -22,11 +27,11 @@ for s in config.sections():
         path = config.get(s, 'Path').split()
         dirs_files.append( (infoName, path, ) )
 
+
 tar = tarfile.open(archiveName, "w:gz")
 
 for infoName, fileList in dirs_files:
     for f in fileList:
-        print infoName + ' : ' + f
         tar.add(f)
 
 tar.close()
@@ -37,3 +42,28 @@ client.connect(hostname = serverUrl, port = serverPort, username = serverUser, p
 trans = scp.SCPClient(client.get_transport())
 trans.put(archiveName)
 client.close()
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Backup " + archiveName + " ended !"
+msg['From'] = sender
+msg['To'] = receiver
+
+text = "Hi!\n Backup Ended."
+html = """\
+<html>
+    <head></head>
+    <body>
+        <h1>Hi!</h1>
+        <h3>Backup Ended.</h3>
+    </body>
+</html>"""
+
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+msg.attach(part1)
+msg.attach(part2)
+
+s = smtplib.SMTP('localhost')
+s.sendmail(sender, receiver, msg.as_string())
+s.quit()
